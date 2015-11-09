03b4de322d5faaf08f6e7d00e7984a00333e0aa9
o-s-e <ose@recommind.com>
2015-11-09 14:57:21 +0100

    added posts controller & index.html

---
+++ static/javascripts/utils/utils.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ose on 11/9/2015.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.utils', [
+            'thinkster.utils.services'
+        ]);
+
+    angular
+        .module('thinkster.utils.services', []);
+})();
